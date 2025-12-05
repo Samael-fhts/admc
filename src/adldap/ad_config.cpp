258bf957e42e159b95bb64b3268ea45d1751f20a
Semyon Knyazev <samael@altlinux.org>
2025-12-05 16:07:43 +0400

    Add sites container dn getter to AdConfig class

--- src/adldap/ad_config.cpp
+++ src/adldap/ad_config.cpp
@@ -171,6 +171,10 @@ QString AdConfig::root_domain_dn() const {
     return d->root_domain_dn;
 }
 
+QString AdConfig::sites_container_dn() const {
+    return QString("CN=Sites,%1").arg(configuration_dn());
+}
+
 QString AdConfig::get_attribute_display_name(const Attribute &attribute, const ObjectClass &objectClass) const {
     if (d->attribute_display_names.contains(objectClass) && d->attribute_display_names[objectClass].contains(attribute)) {
         const QString display_name = d->attribute_display_names[objectClass][attribute];
