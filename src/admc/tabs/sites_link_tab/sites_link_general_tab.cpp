44b74a06ec922179d28e80a5535384a4ac6d8be6
Semyon Knyazev <samael@altlinux.org>
2025-12-15 18:19:15 +0400

    Add sites link/bridge general properties tab widget

---
+++ src/admc/tabs/sites_link_tab/sites_link_general_tab.cpp
@@ -0,0 +1,20 @@
+#include "sites_link_general_tab.h"
+#include "ui_sites_link_general_tab.h"
+#include "sites_link_widget.h"
+#include "attribute_edits/sites_link_edit.h"
+
+SitesLinkGeneralTab::SitesLinkGeneralTab(QList<AttributeEdit *> *edit_list, SitesLinkType type, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::SitesLinkGeneralTab),
+    sites_link_widget(new SitesLinkWidget(type, this)) {
+
+    ui->setupUi(this);
+
+    edit_list->append(new SitesLinkEdit(sites_link_widget, this));
+
+    ui->verticalLayout->addWidget(sites_link_widget);
+}
+
+SitesLinkGeneralTab::~SitesLinkGeneralTab() {
+    delete ui;
+}
