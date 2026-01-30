741d1479c0028722263abbcae178e499903d8a20
Semyon Knyazev <samael@altlinux.org>
2026-01-30 05:04:23 +0400

    Update and rename LogonHoursEdit class to ScheduleHoursEdit
    Rename and update class to take in account site link replication schedule

--- src/admc/attribute_edits/schedule_hours_dialog.h
+++ src/admc/attribute_edits/schedule_hours_dialog.h
@@ -42,9 +42,21 @@ enum Weekday {
     Weekday_COUNT,
 };
 
-#define DAYS_IN_WEEK 7
-#define HOURS_IN_DAY 24
-#define LOGON_HOURS_SIZE (7 * 3)
+// TODO: Split that dialog on two dialogs for user logon and site link
+// schedules
+
+const int DAYS_IN_WEEK = 7;
+const int HOURS_IN_DAY = 24;
+const int LOGON_HOURS_SIZE = 21;
+
+// Site link schedule attribute constants (for site replication)
+const int SITE_LINK_SCHEDULE_TOTAL_SIZE = 188;
+const int SITE_LINK_SCHEDULE_HEADER_SIZE = 20;
+const int SITE_LINK_SCHEDULE_DATA_SIZE = 168;
+
+// Values for schedule bytes
+const char SITE_LINK_SCHEDULE_ALLOWED = (char)0xFF;   // Replication allowed
+const char SITE_LINK_SCHEDULE_DENIED = (char)0xF0;    // Replication denied (or other value you see)
 
 namespace Ui {
 class ScheduleHoursDialog;
@@ -54,9 +66,14 @@ class ScheduleHoursDialog : public QDialog {
     Q_OBJECT
 
 public:
+    enum ScheduleType {
+        ScheduleType_SiteLink,
+        ScheduleType_UserLogon
+    };
+
     Ui::ScheduleHoursDialog *ui;
 
-    ScheduleHoursDialog(const QByteArray &value, QWidget *parent);
+    ScheduleHoursDialog(const QByteArray &value, QWidget *parent, ScheduleType type_arg = ScheduleType_UserLogon);
     ~ScheduleHoursDialog();
 
     QByteArray get() const;
@@ -70,10 +87,25 @@ private:
     void on_local_time_button_toggled(bool checked);
     int get_offset() const;
     bool is_local_time;
+    ScheduleType type;
+
+    const unsigned char sitelink_schedule_header_bytes[20] = {
+        0xBC, 0x00, 0x00, 0x00,  // Size = 188
+        0x00, 0x00, 0x00, 0x00,  // Bandwidth = 0
+        0x01, 0x00, 0x00, 0x00,  // NumSchedules = 1
+        0x00, 0x00, 0x00, 0x00,  // Reserved = 0
+        0x14, 0x00, 0x00, 0x00   // Offset = 20
+    };
 };
 
-QList<QList<bool>> hours_to_bools(const QByteArray &byte_list, const int time_offset = 0);
-QByteArray hours_to_bytes(const QList<QList<bool>> bool_list, const int time_offset = 0);
+QList<QList<bool>> logon_hours_to_bools(const QByteArray &byte_list, const int time_offset = 0);
+QByteArray logon_hours_to_bytes(const QList<QList<bool>> bool_list, const int time_offset = 0);
+
+// Convert site schedule attribute (byte-per-hour format) to bool grid
+QList<QList<bool>> site_schedule_bytes_to_bools(const QByteArray &byte_list_arg, const int time_offset = 0);
+// Convert bool grid to schedule attribute (byte-per-hour format)
+QByteArray bools_to_site_schedule_bytes(const QList<QList<bool>> bool_list, const int time_offset);
+
 int get_current_utc_offset();
 
 #endif /* LOGON_HOURS_DIALOG_H */
