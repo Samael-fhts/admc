0602e8d6e3618c1fab9805a180b9acd8f1f7cc23
Semyon Knyazev <samael@altlinux.org>
2026-02-04 00:22:38 +0400

    Add schedule dialog edit (on button) to site link properties general tab

--- src/admc/tabs/sites_link_tab/sites_link_general_tab.cpp
+++ src/admc/tabs/sites_link_tab/sites_link_general_tab.cpp
@@ -3,6 +3,8 @@
 #include "sites_link_widget.h"
 #include "attribute_edits/sites_link_edit.h"
 #include "attribute_edits/general_name_edit.h"
+#include "attribute_edits/schedule_hours_edit.h"
+#include "sites_link_part_widget.h"
 
 SitesLinkGeneralTab::SitesLinkGeneralTab(QList<AttributeEdit *> *edit_list, SitesLinkType type, QWidget *parent) :
     QWidget(parent),
@@ -13,6 +15,11 @@ SitesLinkGeneralTab::SitesLinkGeneralTab(QList<AttributeEdit *> *edit_list, Site
 
     edit_list->append(new GeneralNameEdit(ui->name_label, this));
     edit_list->append(new SitesLinkEdit(sites_link_widget, this));
+
+    if (sites_link_widget->sites_link_part_widget()) {
+        QPushButton *schedule_button = sites_link_widget->sites_link_part_widget()->schedule_button();
+        edit_list->append(new ScheduleHoursEdit(schedule_button, this));
+    }
 
     ui->verticalLayout->addWidget(sites_link_widget);
 }
