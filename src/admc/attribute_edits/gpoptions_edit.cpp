67b3112773caafd89736feece886f21b9126dfef
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2026-07-10 13:19:19 +0300

    refactor(gpoptions_edit): Remove extra lambdas
    * src/admc/attribute_edits/gpoptions_edit.cpp (GpoptionsEdit::apply): Remove
    extra lambdas.

--- src/admc/attribute_edits/gpoptions_edit.cpp
+++ src/admc/attribute_edits/gpoptions_edit.cpp
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2020-2025 BaseALT Ltd.
  * Copyright (C) 2020-2025 Dmitry Degtyarev
+ * Copyright (C) 2026 Artyom V. Poptsov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -44,15 +45,16 @@ void GpoptionsEdit::load(AdInterface &ad, const AdObject &object) {
 }
 
 bool GpoptionsEdit::apply(AdInterface &ad, const QString &dn) const {
-    const QString new_value = [this]() {
-        const bool checked = check->isChecked();
-        if (checked) {
-            return GPOPTIONS_BLOCK_INHERITANCE;
-        } else {
-            return GPOPTIONS_INHERIT;
-        }
-    }();
-    const bool success = ad.attribute_replace_string(dn, ATTRIBUTE_GPOPTIONS, new_value);
+    const bool checked = check->isChecked();
+    QString new_value;
+    if (checked) {
+        new_value = GPOPTIONS_BLOCK_INHERITANCE;
+    } else {
+        new_value = GPOPTIONS_INHERIT;
+    }
+
+    const bool success =
+        ad.attribute_replace_string(dn, ATTRIBUTE_GPOPTIONS, new_value);
     emit gp_options_changed(check->isChecked());
 
     return success;
