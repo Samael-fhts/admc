de11c41f6c5fe436c1109d80d45ab65cf25b4772
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2026-07-11 09:45:40 +0300

    refactor(changelog_dialog): Improve
    * src/admc/changelog_dialog.h (ChangelogDialog): Add "read_changelog" method.
    * src/admc/changelog_dialog.cpp: (ChangelogDialog::ChangelogDialog): Remove
    extra lambdas; move the change log reading code into a separate method.
    (ChangelogDialog::read_changelog): Implement.

--- src/admc/changelog_dialog.cpp
+++ src/admc/changelog_dialog.cpp
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2020-2025 BaseALT Ltd.
  * Copyright (C) 2020-2025 Dmitry Degtyarev
+ * Copyright (C) 2026 Artyom V. Poptsov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,6 +30,61 @@
 #include <QStandardPaths>
 #include <QFile>
 
+/**
+ * Read a change log file and return its contents.
+ *
+ * @return The change log file contents or an error message on errors.
+ */
+QString ChangelogDialog::read_changelog() const {
+    const QString fail_text = tr("Failed to open changelog file.");
+    const QLocale saved_locale =
+        settings_get_variant(SETTING_locale).toLocale();
+
+    QString changelog_file_name;
+    if (saved_locale.language() == QLocale::Russian) {
+        changelog_file_name = "CHANGELOG_ru.txt";
+    } else {
+        changelog_file_name = "CHANGELOG.txt";
+    }
+
+#ifdef QT_DEBUG
+    QString changelog_path =
+        QString("%1/%2").arg(QCoreApplication::applicationDirPath(),
+                             changelog_file_name);
+#else
+    QString changelog_path =
+        QStandardPaths::locate(
+            QStandardPaths::GenericDataLocation,
+            QString("doc/admc-%1/%2").arg(ADMC_VERSION,
+                                          changelog_file_name));
+#endif
+
+    if (changelog_path.isEmpty()) {
+        return fail_text;
+    }
+
+    QFile file(changelog_path);
+
+    const bool open_success = file.open(QIODevice::ReadOnly);
+    if (!open_success) {
+        qDebug() << "Failed to open changelog file";
+
+        return fail_text;
+    }
+
+    QString changelog_text = file.readAll();
+
+    file.close();
+
+    // Remove forced word wrap contained in
+    // CHANGELOG.txt so that resizing the dialog
+    // expands text width (all wrapped lines start
+    // with 2 spaces)
+    changelog_text.replace("\n  ", " ");
+
+    return changelog_text;
+}
+
 ChangelogDialog::ChangelogDialog(QWidget *parent)
 : QDialog(parent) {
     ui = new Ui::ChangelogDialog();
@@ -36,55 +92,8 @@ ChangelogDialog::ChangelogDialog(QWidget *parent)
 
     setAttribute(Qt::WA_DeleteOnClose);
 
-    const QString changelog_text = []() {
-        const QString fail_text = tr("Failed to open changelog file.");
-
-        const QString changelog_path = []() {
-            const QString changelog_file_name = [&]() {
-                const QLocale saved_locale = settings_get_variant(SETTING_locale).toLocale();
-
-                if (saved_locale.language() == QLocale::Russian) {
-                    return "CHANGELOG_ru.txt";
-                } else {
-                    return "CHANGELOG.txt";
-                }
-            }();
-
-#ifdef QT_DEBUG
-            return QString("%1/%2").arg(QCoreApplication::applicationDirPath(), changelog_file_name);
-#endif
-
-            return QStandardPaths::locate(QStandardPaths::GenericDataLocation, QString("doc/admc-%1/%2").arg(ADMC_VERSION, changelog_file_name));
-        }();
-
-        if (changelog_path.isEmpty()) {
-            return fail_text;
-        }
-
-        QFile file(changelog_path);
-
-        const bool open_success = file.open(QIODevice::ReadOnly);
-        if (!open_success) {
-            qDebug() << "Failed to open changelog file";
-
-            return fail_text;
-        }
-
-        QString out = file.readAll();
-
-        file.close();
-
-        // Remove forced word wrap contained in
-        // CHANGELOG.txt so that resizing the dialog
-        // expands text width (all wrapped lines start
-        // with 2 spaces)
-        out.replace("\n  ", " ");
-
-        return out;
-    }();
-
+    QString changelog_text = read_changelog();
     ui->edit->setPlainText(changelog_text);
-
     settings_setup_dialog_geometry(SETTING_changelog_dialog_geometry, this);
 }
 
